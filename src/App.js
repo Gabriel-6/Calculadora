b1dfb1309dce44dd4a32bec50cef89997bd3e7d3
Gabriel-6 <116814159+Gabriel-6@users.noreply.github.com>
2024-03-27 16:47:23 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,30 +4,30 @@ import './index.css'
 function App() {
   const [displayValue, setDisplayValue] = useState('0');
 
-  
+
 
   return (
     <div className="bg-gray-500 w-80 mx-auto my-10 p-4 rounded-lg shadow-lg">
-      <input className="w-full text-right text-2xl bg-transparent text-white border-1 border-cyan-500" type='text' value={displayValue} disabled />
+      <input className="w-full text-right pr-3 text-2xl bg-white text-black border-1 rounded-lg border-cyan-500" type='text' value={displayValue} disabled />
 
-      <div className="grid grid-cols-4 gap-4 mt-4">
-        <button className="transition duration-200 active: translate-x-1 col-span-2 text-xl">AC</button>
+      <div className="grid grid-cols-4 gap-4 mt-4 text-white">
+        <button className="transition duration-200 active: translate-x-1 col-span-2 text-xl" onClick={() => setDisplayValue("0")}>AC</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">Del</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">/</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">7</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">8</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">9</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("7")}>7</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("8")}>8</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("9")}>9</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">*</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">4</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">5</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">6</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("4")}>4</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("5")}>5</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("6")}>6</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">-</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">1</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">2</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">3</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("1")}>1</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("2")}>2</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("3")}>3</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">+</button>
         <button className="transition duration-200 active: translate-x-1 text-xl" disabled></button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">0</button>
+        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setDisplayValue("0")}>0</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">.</button>
         <button className="transition duration-200 active: translate-x-1 text-xl">=</button>
       </div>
