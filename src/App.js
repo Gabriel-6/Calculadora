0bf8713c203cac84bbf769ea371964efa72d58be
Gabriel-6 <116814159+Gabriel-6@users.noreply.github.com>
2024-04-05 12:54:58 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ function App() {
 
   const setNumber = (number) => {
     if(selectedOperator) {
-      setDisplayValue(displayValue + number)
+      setDisplayValue(displayValue === '0' || displayValue === 'Não Possivel Calcular' ? number: displayValue + number)
       setSelectOperator(false)
     }else{
-      setDisplayValue(displayValue === '0' ? number: displayValue + number)
+      setDisplayValue(displayValue === '0' || displayValue === 'Não Possivel Calcular' ? number: displayValue + number)
     }
   }
 
   const setOperation = (operation) => {
-    if(!selectedOperator){
+    const lastChar = displayValue[displayValue.length - 1]
+    if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+      setDisplayValue(displayValue.slice(0, -1) + operation)
+    } else {
       setDisplayValue(displayValue + operation)
+    }
+    setSelectOperator(true)
+  }
+
+  const setDot = (dot) => {
+    if(!selectedOperator){
+      setDisplayValue(displayValue + dot)
       setSelectOperator(true)
     }
   }
@@ -27,39 +37,60 @@ function App() {
   }
 
   const deleteDigit = () => {
-    setDisplayValue(displayValue.slice(0, (displayValue.length-1)))
+    if(typeof displayValue === 'string' && displayValue.length > 0) {
+      setDisplayValue(displayValue.slice(0, -1))
+    }
   }
 
+  const verifyNumber = (number) => {
+    return String(number).includes('.') ? true : false
+  }
+
+
   const calculate = () => {
-    setDisplayValue(eval(displayValue).toFixed(2))
+    try{
+      const result = eval(displayValue)
+      if(result === Infinity || isNaN(result)){
+        setDisplayValue("Não Possivel Calcular")
+      }
+      else{
+        if(verifyNumber(result)) {
+          setDisplayValue(String(result.toFixed(2)))
+        } else {
+          setDisplayValue(String(result))
+        }
+      }
+    }catch(error){
+      setDisplayValue('Não Possivel Calcular')
+    } 
   }
 
  
 
   return (
     <div className="bg-gray-500 w-80 mx-auto my-10 p-4 rounded-lg shadow-lg">
-      <input className="w-full text-right pr-3 text-2xl bg-white text-black border-1 rounded-lg border-cyan-500" type='text' value={displayValue} disabled />
+      <input className="w-full text-right pr-3 text-2xl bg-white text-black border-1 rounded-lg h-16 border-cyan-500" type='text' value={displayValue} disabled />
 
-      <div className="grid grid-cols-4 gap-4 mt-4 text-white">
-        <button className="transition duration-200 active: translate-x-1 col-span-2 text-xl" onClick={() => clearDisplay()}>AC</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => deleteDigit()}>Del</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setOperation('/')}>/</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("7")}>7</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("8")}>8</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("9")}>9</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setOperation('*')}>*</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("4")}>4</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("5")}>5</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("6")}>6</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setOperation('-')}>-</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("1")}>1</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("2")}>2</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("3")}>3</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setOperation('+')}>+</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" disabled></button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => setNumber("0")}>0</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl">.</button>
-        <button className="transition duration-200 active: translate-x-1 text-xl" onClick={() => calculate()}>=</button>
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        <button className="col-span-2 text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => clearDisplay()}>AC</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => deleteDigit()}>Del</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setOperation('/')}>/</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("7")}>7</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("8")}>8</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("9")}>9</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setOperation('*')}>*</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("4")}>4</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("5")}>5</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("6")}>6</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setOperation('-')}>-</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("1")}>1</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("2")}>2</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("3")}>3</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setOperation('+')}>+</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" disabled></button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setNumber("0")}>0</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => setDot(".")}>.</button>
+        <button className="text-xl py-3 bg-gray-800 text-white rounded-lg" onClick={() => calculate()}>=</button>
       </div>
     </div>
   );
